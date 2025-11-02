02d7e8a15cc82e8569eb5ece975136dd09efbb0e
iy-sketch <aliyildirim500a@gmail.com>
2025-11-02 10:37:57 +0300

    Vite yapılandırması düzeltildi

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sinav-calar-saati/',
+  base: '/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  server: {
+    port: 3000
+  }
 })
-
